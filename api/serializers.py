a9a1a695ab931f563cab602a6a9b9773863623a0
hitzseb <hitzseb@gmail.com>
2023-11-04 22:17:35 -0300

    serializers

--- api/serializers.py
+++ api/serializers.py
@@ -22,6 +22,11 @@ class ArcSerializer(serializers.ModelSerializer):
         model = Arc
         fields = ['number', 'title', 'plot', 'saga']
         
+class ArcSerializerMin(serializers.ModelSerializer):
+    class Meta:
+        model = Arc
+        fields = ['number', 'title']
+        
 class MangaListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Manga
@@ -36,15 +41,38 @@ class VolumeSerializer(serializers.ModelSerializer):
     manga = MangaListSerializer(many=True, read_only=True)
     class Meta:
         model = Volume
-        fields = ['number', 'date', 'cover', 'manga']
+        fields = ['number', 'title', 'date', 'plot', 'cover', 'manga']
+        
+class VolumeSerializerMin(serializers.ModelSerializer):
+    class Meta:
+        model = Volume
+        fields = ['number', 'title', 'cover']
         
 class MangaDetailSerializer(serializers.ModelSerializer):
+    volume = VolumeSerializerMin(many=False, read_only=True)
+    arc = ArcSerializerMin(many=False, read_only=True)
     characters = CharacterListSerializer(many=True, read_only=True)
     class Meta:
         model = Manga
         fields = ['number', 'title', 'date', 'cover', 'plot', 'volume', 'arc', 'characters']
         
+class MangaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manga
+        fields = ['number', 'title', 'date', 'cover', 'plot', 'volume', 'arc']
+        
+class MangaSerializerMin(serializers.ModelSerializer):
+    class Meta:
+        model = Manga
+        fields = ['number', 'title', 'cover']
+        
 class CharacterDetailSerializer(serializers.ModelSerializer):
+    manga_debut = MangaSerializerMin(many=False, read_only=True)
+    class Meta:
+        model = Character
+        fields = ['name', 'picture', 'description', 'species', 'status', 'manga_debut']
+        
+class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ['name', 'picture', 'description', 'species', 'status', 'manga_debut']
--- api/views.py
+++ api/views.py
@@ -70,12 +70,12 @@ class MangaRetrieveView(RetrieveAPIView):
 @method_decorator(user_passes_test(is_admin), name='dispatch')
 class MangaCreateView(CreateAPIView):
     queryset = Manga.objects.all()
-    serializer_class = MangaDetailSerializer
+    serializer_class = MangaSerializer
 
 @method_decorator(user_passes_test(is_admin), name='dispatch')
 class MangaRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Manga.objects.all()
-    serializer_class = MangaDetailSerializer
+    serializer_class = MangaSerializer
     lookup_field = 'number'
     
 # Character views
@@ -92,10 +92,10 @@ class CharacterRetrieveView(RetrieveAPIView):
 @method_decorator(user_passes_test(is_admin), name='dispatch')
 class CharacterCreateView(CreateAPIView):
     queryset = Character.objects.all()
-    serializer_class = CharacterDetailSerializer
+    serializer_class = CharacterSerializer
 
 @method_decorator(user_passes_test(is_admin), name='dispatch')
 class CharacterRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Character.objects.all()
-    serializer_class = CharacterDetailSerializer
+    serializer_class = CharacterSerializer
     lookup_field = 'name'
